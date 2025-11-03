3dcfac247fcaf7462d484c82cc73613994b3f6ab
Alex Mosiychuk <oleksiymosiychuk108@gmail.com>
2025-11-04 00:20:12 +0300

    Update catalog/urls.py
    Co-authored-by: painassasin <painassasin@icloud.com>

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,7 +7,7 @@ app_name = "catalog"
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("products_list/", views.products_list, name="products_list"),
-    path("product_info/<int:product_id>", views.product_info, name="product_info"),
+    path("product_info/<int:product_id>/", views.product_info, name="product_info"),
     path("categories/", views.categories, name="categories"),
     path("contacts/", views.contacts, name="contacts"),
 ]
