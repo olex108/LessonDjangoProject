fbf7708f0179fb3e53b294218c7b623fa08b83f4
Alex <oleksiymosiychuk108@gmail.com>
2025-11-07 13:57:34 +0300

    update urls for catalog app

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "catalog"
 
 urlpatterns = [
-    path("home/", views.HomeView.as_view(), name="home"),
-    path("products_list/", views.ProductsListView.as_view(), name="products_list"),
-    path("product_detail/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
-    path("categories/", views.CategoriesListView.as_view(), name="categories"),
-    path("contacts/", views.ContactsView.as_view(), name="contacts"),
+    path("catalog/home/", views.HomeView.as_view(), name="home"),
+    path("catalog/products_list/", views.ProductsListView.as_view(), name="products_list"),
+    path("catalog/product_detail/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
+    path("catalog/categories/", views.CategoriesListView.as_view(), name="categories"),
+    path("catalog/contacts/", views.ContactsView.as_view(), name="contacts"),
 ]
