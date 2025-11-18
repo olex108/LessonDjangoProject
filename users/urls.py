d607b9a4d93b729fc306c367e34de236390411c6
Alex <oleksiymosiychuk@gmail.com>
2025-11-18 23:51:09 +0300

    update users app

---
+++ users/urls.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+from . import views
+from .forms import CustomAuthenticationForm
+
+app_name = "users"
+
+urlpatterns = [
+    path("registration/", views.UserRegistrationView.as_view(), name="registration"),
+    path(
+        "login/",
+        LoginView.as_view(template_name="users/login.html", authentication_form=CustomAuthenticationForm),
+        name="login",
+    ),
+    path("logout/", LogoutView.as_view(next_page="catalog:home"), name="logout"),
+    path("users/email_confirm/<str:token>/", views.email_verification, name="email_confirm"),
+]
